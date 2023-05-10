b4411234d3f8dd2a2066e497191c683c867932cf
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-05-10 18:21:31 -0500

    Main section header

--- src/features/Tasks/Tasks.tsx
+++ src/features/Tasks/Tasks.tsx
@@ -1,4 +1,26 @@
+import { IconContext } from "react-icons";
+import { GoPlus } from "react-icons/go";
+import { MdIosShare } from "react-icons/md";
+
+import s from "./Tasks.module.scss";
+
 const Tasks = () => {
-  return <div>Tasks</div>;
+  return (
+    <main className={s.main}>
+      <header className={s.header}>
+        <div className={s.headerButtons}>
+          <IconContext.Provider value={{ size: "25px" }}>
+            <MdIosShare className={s.icon} />
+            <GoPlus className={s.icon} />
+          </IconContext.Provider>
+        </div>
+        <div className={s.headerTitle}>
+          <h1 className={s.title}>Reminders</h1>
+          <h1 className={s.count}>4</h1>
+        </div>
+      </header>
+      <ul></ul>
+    </main>
+  );
 };
 export default Tasks;
